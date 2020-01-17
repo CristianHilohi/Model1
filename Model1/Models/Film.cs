5f54a3bc96e56f3aef2a1e434b588fb8349b1cc2
CristianHilohi <48750997+CristianHilohi@users.noreply.github.com>
2020-01-17 16:26:01 +0200

    Add project files.

---
+++ Model1/Models/Film.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Model1.Models
+{
+    public class Film
+    {
+        private FilmDBContext db = new FilmDBContext();
+    }
+
+    public class FilmDBContext : DbContext
+    {
+        public FilmDBContext() : base("DBConnectionString") { }
+        public DbSet<Film> Filme { get; set; }
+    }
+}
---
+++ Model1/Models/Recenzie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Model1.Models
+{
+    public class Recenzie
+    {
+    }
+
+    public class RecenzieDBContext : DbContext
+    {
+        public RecenzieDBContext() : base("DBConnectionString") { }
+        public DbSet<Recenzie> Recenzii { get; set; }
+    }
+}
